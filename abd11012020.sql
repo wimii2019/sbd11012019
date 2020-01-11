0ff0c047b5300e44ec562e9302fc2a5d2b876e7e
wimii2019 <56503354+wimii2019@users.noreply.github.com>
2020-01-11 08:33:57 +0100

    c2

--- abd11012020.sql
+++ abd11012020.sql
@@ -6,3 +6,19 @@ Create or REPLACE TYPE t_lek as OBJECT(
   lek_procent_refundacji NUMBER(3,2),
   MEMBER FUNCTION SHOW RETURN VARCHAR2
 );
+/
+
+CREATE OR REPLACE TYPE BODY t_lek AS MEMBER FUNCTION SHOW RETURN VARCHAR2 AS
+v_all VARCHAR2(150);
+  BEGIN 
+  v_all := lek_id||' '||lek_nazwa||' '||lek_wymagana_spec_lekarza||' '||lek_cena_calosc||' '||lek_procent_refundacji;
+  RETURN v_all;
+  END;
+  END;
+  /
+  
+CREATE TABLE lek_objs of t_lek;
+
+INSERT INTO lek_objs VALUES(t_lek(1,'Gripex',2,5.99,0.51));
+
+SELECT * FROM lek_objs;
